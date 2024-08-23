2a827a746ee8fbc35926ca20b2aa812918cb39ee
tiagomatsushima <dev.matsu22@gmail.com>
2024-08-23 12:43:00 -0300

    Formulário implementado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace SupplyChainManager.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
